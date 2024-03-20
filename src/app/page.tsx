83bea59c18610560be2bb36f14b9c3869533223c
hebre0865 <hebre0865@uib.no>
2024-03-20 17:49:39 +0100

    hei

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,8 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center h-full gap-10 p-24">
-      <h1 className="text-3xl font-medium">
-        Min tetris er bedre en tonys!
-      </h1>
-
-      <p>
-        Om du vil ha oppgaver, les{" "}
-        <span className="bg-gray-200 font-mono py-1 px-2 rounded-md">
-          README.md
-        </span>
-        .
-      </p>
-    </main>
+    <div className="text-center">
+    <h1 className="text-2xl">Hei, jeg heter [ditt navn]!</h1>
+    <p>Jeg studerer [studie] ved Universitetet i Bergen.</p>
+  </div>
   );
 }
