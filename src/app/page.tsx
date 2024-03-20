d3a6189f9e9e3be6e077131682f3d6d05374b297
hebre0865 <hebre0865@uib.no>
2024-03-20 17:52:11 +0100

    hei

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div className="text-center">
-    <h1 className="text-2xl">Hei, jeg heter [ditt navn]!</h1>
-    <p>Jeg studerer [studie] ved Universitetet i Bergen.</p>
+    <h1 className="text-2xl">Hei, jeg heter Henrik!</h1>
+    <p>Jeg studerer Datasikkerhet ved Universitetet i Bergen.</p>
   </div>
   );
 }
