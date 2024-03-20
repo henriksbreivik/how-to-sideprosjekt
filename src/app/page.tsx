ac3b6c6c937844553fd03a5ce7bcef18aad103bf
hebre0865 <hebre0865@uib.no>
2024-03-20 17:58:49 +0100

    hei

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="text-center">
+    <div className="text-center mt-4">
     <h1 className="text-2xl">Hei, jeg heter Henrik!</h1>
     <p>Jeg studerer Datasikkerhet ved Universitetet i Bergen.</p>
   </div>
